3a31f62e93c9a24eced03286d6561b3cc067f357
ilya <I.Rogachev@gazprom-auto.ru>
2024-11-30 23:51:21 +0300

    ops(styles): added tailwind + shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, './src/app'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@entities': path.resolve(__dirname, './src/entities'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@widgets': path.resolve(__dirname, './src/widgets'),
-      '@pages': path.resolve(__dirname, './src/pages'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
