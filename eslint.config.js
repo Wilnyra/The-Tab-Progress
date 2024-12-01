128079639286c303ee580d0b2b5ed1432b85cf47
ilya <I.Rogachev@gazprom-auto.ru>
2024-12-01 15:22:03 +0300

    ops: eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintPluginImport from 'eslint-plugin-import'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': eslintPluginImport
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +25,24 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'import/order': [
+        'error',
+        {
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'alphabetize': {
+            'order': 'asc',
+            'caseInsensitive': true
+          }
+        }
+      ],
+      'max-len': ['error', { 
+        'code': 80,
+        'tabWidth': 2,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreRegExpLiterals': true
+      }]
     },
   },
 )
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import { appName } from "@/shared/lib/constants"
-import { Button } from "@/shared/ui/button"
+import { Button } from "@/shared/ui/Button"
 import "./index.css"
 
 function App() {
   return (
-    <div>
-      <h1>{appName} v0.0.0</h1>
-      <Button>Click me</Button>
+    <div className="dark bg-background">
+      <h1 className="text-foreground">{appName} v0.0.0</h1>
+      <Button variant="secondary">Click me</Button>
     </div>
   )
 }
