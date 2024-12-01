84fe7a4dbf1b75ccccbe2d7df60fda8f580b9d71
ilya <I.Rogachev@gazprom-auto.ru>
2024-12-01 17:31:52 +0300

    feat(auth): supabase otp auth

---
+++ src/features/auth/login/index.ts
@@ -0,0 +1,1 @@
+export { OtpAuthForm } from "./ui/OtpAuthForm"
---
+++ src/features/auth/login/ui/OtpAuthForm.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { supabase } from '@/entities/session'
+import { Button } from '@/shared/ui/Button'
+import { Input } from '@/shared/ui/Input'
+
+export const OtpAuthForm = () => {
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    setLoading(true)
+    const { error } = await supabase.auth.signInWithOtp({ email })
+    if (error) {
+      alert(error.message)
+    } else {
+      alert('Check your email for the login link!')
+    }
+    setLoading(false)
+  }
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Supabase + React</h1>
+        <p className="description">Sign in via magic link with your email below</p>
+        <form className="form-widget" onSubmit={handleLogin}>
+          <div>
+            <Input
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <Button className={'button block'} disabled={loading}>
+              {loading ? <span>Loading</span> : <span>Send magic link</span>}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
